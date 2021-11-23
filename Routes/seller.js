1f34d68bb01f80e884a4304d249cab56547011da
Usman-Ghani-Mughal <usmangm087@gmail.com>
2021-11-23 17:26:04 +0500

    handle the images

--- Routes/seller.js
+++ Routes/seller.js
@@ -13,32 +13,7 @@ const verifyToken = require('../Validations/verifyToken');
 dotenv.config();
 
 // for uploding files.
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, './images/');
-    },
-    filename: function(req, file, cb){
-        cb(null, new Date().toISOString() + file.originalname);
-    }
-});
-
-const fileFilter = (req, file, cb) =>{
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null, true);
-    }else{
-        cb(new Error('file size or type is not valid'),false);
-    }
-}
-
-const upload = multer({
-    storage: storage, 
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-});
+const upload = require("../Middleware/uploadimage");
 
 
 
