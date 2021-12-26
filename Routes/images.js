38cf662411b3b6e540dee06ab7c24ef941158ecf
Usman-Ghani-Mughal <usmangm087@gmail.com>
2021-12-27 00:37:07 +0500

    aws sdk images

--- Routes/images.js
+++ Routes/images.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const {getFileStream } = require('../Middleware/up_down_image_s3');
 
 router.get('/get/:key', (req, res) => {
+    console.log("---***********--------**********----------");
+    console.log(req.params.key);
     const key = req.params.key
     const readStream = getFileStream(key)
     readStream.pipe(res)
+    console.log("---***********--------**********----------");
   })
 
 module.exports = router;
